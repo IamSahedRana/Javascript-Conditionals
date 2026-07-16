c175d44c2cfae489cd89236767ed8a13e5cfdc12
IamSahedRana <sahed.vmc@gmail.com>
2026-07-16 22:36:35 +0600

    Ternary Operator

---
+++ ternary.js
@@ -0,0 +1,13 @@
+let price = 500;
+let isleader = false;
+
+price = isleader ? 0 : price +100;
+
+console.log("Price Now:", price)
+
+
+let age = 20 ;
+
+let message = age >= 18 ? "Adult" : "Minor";
+
+console.log(message);
