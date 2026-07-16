03e4482ac009e347931ffedbdcf0a9e26a36141c
IamSahedRana <sahed.vmc@gmail.com>
2026-07-16 13:49:56 +0600

    Learn Javascript IF Statement

---
+++ if.js
@@ -0,0 +1,13 @@
+/* Learn If Statement  */
+
+const price = 5000;
+const money = 4999;
+
+if (price >= money){
+     console.log('He is Rich')
+}
+
+const isRich = true;
+if (isRich === true){
+     console.log("He is Really Rich")
+}
