94aed8911254c0be0ec42abc3aa2f2bc595bb9bc
IamSahedRana <sahed.vmc@gmail.com>
2026-07-16 17:38:16 +0600

    Multiple Condition Logical Operators

---
+++ logic.js
@@ -0,0 +1,10 @@
+let salary = 50000;
+let age = 30;
+let have_govt_job = true;
+
+if ((salary >45000 && age <= 30) || have_govt_job){
+     console.log("Marry My DAUGHTER");
+}
+else {
+     console.log("NAH!, You Wouldn't Get Her!");
+}
