aadd8acc5f3c5627df1825362e02ce415aef8891
IamSahedRana <sahed.vmc@gmail.com>
2026-07-16 13:58:57 +0600

    Forget to use semicolon at last of the statement

--- if.js
+++ if.js
@@ -4,10 +4,10 @@ const price = 5000;
 const money = 4999;
 
 if (price >= money){
-     console.log('He is Rich')
+     console.log('He is Rich');
 }
 
 const isRich = true;
 if (isRich === true){
-     console.log("He is Really Rich")
+     console.log("He is Really Rich");
 }
