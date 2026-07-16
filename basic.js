1f9d6375863d54c43c649c8dfa9eeda4fb0295ef
IamSahedRana <sahed.vmc@gmail.com>
2026-07-16 13:21:50 +0600

    Basic Javascript Opeators

---
+++ basic.js
@@ -0,0 +1,12 @@
+ a = 39;
+ b = 49;
+
+ console.log( a < b);
+ console.log( a > b);
+ console.log( a <= b);
+ console.log( a >= b);
+ console.log( a === b);
+ console.log( a !== b);
+
+//  And = && 
+//  OR = ||
